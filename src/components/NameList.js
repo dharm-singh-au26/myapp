e471a4b6a2fb53d1b3df0f56fe78098f00134218
Dharm Singh <dharmjat45@gmail.com>
2022-10-05 14:36:58 +0530

    List Rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Person from './Person'
+
+function NameList() {
+    const persons = [
+        {
+            id : 1,
+            name : 'Dharm',
+            age : 28,
+            skills : 'React'
+        },
+        {
+            id : 2,
+            name : 'bablu',
+            age : 25,
+            skills : 'JavaScript'
+        },
+        {
+            id : 3,
+            name : 'Sunil',
+            age : 22,
+            skills : 'NodeJS'
+        },
+        {
+            id : 4,
+            name : 'Harsh',
+            age : 30,
+            skills : 'MongoDB'
+        }
+    ]
+    const personList = persons.map(person=> (
+        <Person person={person} ></Person>
+    ))
+  return (
+    <div>
+      {personList}
+    </div>
+  )
+}
+
+export default NameList
