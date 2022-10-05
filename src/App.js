cfdcc6c47e2ab790ff54ca568514c2b9c454836f
Dharm Singh <dharmjat45@gmail.com>
2022-10-05 11:12:59 +0530

    eventHandling

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick/>
+      <ClassClick/>
       {/* <Counter/> */}
 
       {/* <Message/> */}
-      <Greet name='Bruce' heroName='batman'>
+      {/* <Greet name='Bruce' heroName='batman'>
         <p>This is a children tag</p>
       </Greet>
       <Greet name='live' heroName='spiderman'>
         <button>Action</button>
-      </Greet>
+      </Greet> */}
        {/* class component */}
-     <Welcome name='Bruce' heroName='batman'/>
+     {/* <Welcome name='Bruce' heroName='batman'/>
       <Welcome name='live' heroName='spiderman'/>
       <Welcome name='melody' heroName='choclate'/>
-      <Hello/> 
+      <Hello/>  */}
 
       
     </div>
