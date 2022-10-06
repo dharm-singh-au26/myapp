681624885a9356d6ecbb343704db65ddb558a454
Dharm Singh <dharmjat45@gmail.com>
2022-10-06 17:46:20 +0530

    Forword Ref

--- src/App.js
+++ src/App.js
@@ -20,11 +20,15 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
-      <ParentComp/>
+      <FRParentInput/>
+      {/* <RefsDemo/> */}
+      {/* <ParentComp/> */}
       {/* <Table/> */}
       {/* <FragmentDemo/> */}
       {/* <LifecycleA/> */}
