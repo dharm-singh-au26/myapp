b95dffc05fa7ab0aef1354cc2792790936b4a00c
Dharm Singh <dharmjat45@gmail.com>
2022-10-07 23:12:15 +0530

    Error boundary

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import './App.css'
- 
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import InLine from './components/InLine';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
+import React from "react";
+import "./App.css";
+
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import InLine from "./components/InLine";
+import Form from "./components/Form";
+import LifecycleA from "./components/LifecycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
+import PureComp from "./components/PureComp";
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
+      <ErrorBoundary>
+        <Hero heroName="Batman"></Hero>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman"></Hero>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker"></Hero>
+      </ErrorBoundary>
       {/* <PortalDemo/> */}
       {/* <FRParentInput/> */}
       {/* <RefsDemo/> */}
@@ -52,13 +63,11 @@ function App() {
       <Greet name='live' heroName='spiderman'>
         <button>Action</button>
       </Greet> */}
-       {/* class component */}
-     {/* <Welcome name='Bruce' heroName='batman'/>
+      {/* class component */}
+      {/* <Welcome name='Bruce' heroName='batman'/>
       <Welcome name='live' heroName='spiderman'/>
       <Welcome name='melody' heroName='choclate'/>
       <Hello/>  */}
-
-      
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hero({ heroName }) {
+  if (heroName === "Joker") {
+    throw new Error("not a Hero");
+  }
+  return <div>{heroName}</div>;
+}
+
+export default Hero;
